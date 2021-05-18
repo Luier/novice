a3cbc5aec6efa260c0ab34fa4d1a551b7a0490a3
21407 <2140784066@qq.com>
2021-05-18 17:27:46 +0800

    默认的

---
+++ src/helloworld.java
@@ -0,0 +1,10 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+/**
+ * @create $(MONTH)-$(DAY)-$(TIME)
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
